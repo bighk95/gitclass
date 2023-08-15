5136d5899ce71960dfb70c70b23ecebbd9fc9e4f
bighk95 <wnsvmfh77@gmail.com>
2023-08-15 14:02:52 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello, Javascript")
+
+var age = 25;
+let year = 2023;    // 문자, 숫자
+const point = 250;
+const today = "광복절"; // 문자
+
+console.log(age);
+console.log(year);
+console.log(point);
+console.log(today);
+
+
+// Data type
+
+// Number = 1,2,3,4,5,6,7,8,9,0
+// String = 'hello world'
